bfb3979fd7186773bc16f687495e4d852fe3421a
petrussampaio <petrussampaiio@gmail.com>
2024-08-13 19:29:25 -0300

    arquivo faltante

--- ex01/features/steps/classificar_triangulo_steps.py
+++ ex01/features/steps/classificar_triangulo_steps.py
@@ -1,15 +1,17 @@
 from behave import Given, When, Then
+from Triangulo import Triangulo
 
 @Given("os lados do triângulo {a}, {b}, {c}")
 def step_given_lados_do_triangulo(context, a, b, c):
-    context.a = a
-    context.b = b
-    context.c = c
+    context.a = int(a)
+    context.b = int(b)
+    context.c = int(c)
 
 @When("eu classifico o triângulo")
-def step_given_lados_do_triangulo(context):
-    context.resultado = "Equilátero"
+def step_when_classifico_o_triangulo(context):
+    triangulo = Triangulo(context.a, context.b, context.c)
+    context.resultado = triangulo.tipo_triangulo()
 
 @Then("o resultado deve ser {esperado}")
-def set_then_o_resultado_deve_ser(context, esperado):
+def step_then_o_resultado_deve_ser(context, esperado):
     assert context.resultado == esperado, f"Esperado {esperado}, mas obteve {context.resultado}"
